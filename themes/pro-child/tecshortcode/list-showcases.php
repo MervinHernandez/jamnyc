37070f3597fe748d5d8667022982e2309394997e
Mervin Hernandez <me@mervinhernandez.com>
2017-12-14 03:51:27 -0500

    STARTED working on the new list shortcode template

---
+++ themes/pro-child/tecshortcode/list-showcases.php
@@ -0,0 +1,14 @@
+<?php $contentorder = array_map( 'trim', $atts['contentorder'] ); ?>
+
+<!-- Raw CONTENT -->
+<div class="ecs-event">
+		<div class="month" id="topevents-month">
+			<?php echo tribe_get_start_date( null, false, 'l' ) ?>
+		</div>
+		<div class="ecs-title">
+			<a href="<?php echo tribe_get_event_link(); ?>" rel="bookmark"><?php echo apply_filters( 'ecs_event_list_title', get_the_title(), $atts, $post ) ?></a>
+		</div>
+		<div class="day" id="topevents-day">
+			<?php echo tribe_get_start_date( null, false, 'M j' ).' @ '.tribe_get_start_date( null, false, 'g:i a' )?>
+		</div>
+	</div>
