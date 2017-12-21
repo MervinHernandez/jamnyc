ae7a4c7a5da62eb66283add690015abcc447ef6c
Mervin Hernandez <me@mervinhernandez.com>
2017-12-20 23:53:36 -0500

    ADDED ACF Custom Settings Page

--- themes/pro-child/functions.php
+++ themes/pro-child/functions.php
@@ -18,6 +18,23 @@
 
 add_filter( 'x_enqueue_parent_stylesheet', '__return_true' );
 
+// ACF - Custom Gear List Page
+add_action('acf/init', 'my_acf_init');
+
+function my_acf_init() {
+
+	if( function_exists('acf_add_options_page') ) {
+
+		$option_page = acf_add_options_page(array(
+			'page_title' 	=> __('Jam NYC', 'acf_jamnyc'),
+			'menu_title' 	=> __('Jam NYC ACF', 'acf_jamnyc'),
+			'menu_slug' 	=> 'acf-jamnyc',
+		));
+
+	}
+
+}
+
 
 // Additional Functions
 // =============================================================================
