6fbe1ce8d39aaab7b2faea7e40c9fcb3673839f3
Mervin Hernandez <me@mervinhernandez.com>
2018-01-18 03:14:53 -0500

    Registered piklist plugin

---
+++ plugins/pk-jamnyc/parts/dashboard/dash-mervin.php
@@ -0,0 +1,7 @@
+<?php
+/*
+Title: Welcome to The Jam NYC
+Capability: manage_options
+*/
+
+_e('This site is managed by Mervin Hernandez', 'piklist');
---
+++ plugins/pk-jamnyc/pk-jamnyc.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* Plugin Name: Piklist - Jam NYC Customizations
+* Plugin URI: https://mervinhernandez.com
+* Description: These are custom functions created on Piklist
+* Version: 1.0.0
+* Author: Mervin Hernandez
+* Author URI: https://mervinhernandez.com
+* Plugin Type: Piklist
+*/
+
+
