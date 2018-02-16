3c67d30d8dc5133e90b7361f6a5424555c41e69c
Mervin Hernandez <me@mervinhernandez.com>
2018-02-15 19:59:37 -0500

    added my contact information to the dashboard

--- plugins/pk-jamnyc/parts/dashboard/dash-mervin.php
+++ plugins/pk-jamnyc/parts/dashboard/dash-mervin.php
@@ -4,4 +4,4 @@ Title: Welcome to The Jam NYC
 Capability: manage_options
 */
 
-_e('This site is managed by Mervin Hernandez', 'piklist');
+_e('This site is managed by <strong>Mervin Hernandez</strong><br>for assistance, please contact me at<br>(407) 432-3825 or me@mervinhernandez.com', 'piklist');
