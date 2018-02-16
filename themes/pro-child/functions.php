bd4bcde6b83e50aaf59bcc6cc5f50e6a7cc30e5e
Mervin Hernandez <me@mervinhernandez.com>
2018-02-16 03:04:50 -0500

    FUNCTIONS - deleted ACF stuff and hid some messages

--- themes/pro-child/functions.php
+++ themes/pro-child/functions.php
@@ -18,41 +18,13 @@
 
 add_filter( 'x_enqueue_parent_stylesheet', '__return_true' );
 
+// HIDE Messages
+// =============================================================================
+add_filter( 'woocommerce_helper_suppress_admin_notices', '__return_true' );
+
+
 // Additional Functions
 // =============================================================================
-
-// ACF - Custom Gear List Page
-add_action('acf/init', 'my_acf_init');
-function my_acf_init() {
-
-	if( function_exists('acf_add_options_page') ) {
-
-		$option_page = acf_add_options_page(array(
-			'page_title' 	=> __('Jam NYC', 'acf_jamnyc'),
-			'menu_title' 	=> __('Jam NYC ACF', 'acf_jamnyc'),
-			'menu_slug' 	=> 'acf-jamnyc',
-		));
-
-	}
-
-}
-
-// ACF - DISPLAY Sponsors - Shortcode Registration
-function jamspons() {
-	$jamspons = '';
-	if( have_rows('sponsor_item','option') ):
-		while ( have_rows('sponsor_item','option') ) : the_row();
-			$jamspons .= get_sub_field('sponsor_name').'</br>';
-			$jamspons .= '<img style="width:80px;" src="'.get_sub_field('sponsor_logo').'"/> </br>';
-			$jamspons .= get_sub_field('sponsor_link').'</br>';
-		endwhile;
-	else :
-		// no rows found
-	endif;
-	// Done
-	return $jamspons;
-}
-add_shortcode( 'acf-jamnyc-sponsors', 'jamspons' );
 
 // WOO - Exclude Category from Shop Page
 function custom_pre_get_posts_query( $q ) {
