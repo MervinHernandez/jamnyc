ed22b2c0a936a63492e3f79da92a87fa537b6a12
Mervin Hernandez <me@mervinhernandez.com>
2018-03-20 02:10:15 -0400

    GRAVITY FORMS - User Registration - Custom Activation Page

--- themes/pro-child/functions.php
+++ themes/pro-child/functions.php
@@ -25,6 +25,24 @@ add_filter( 'woocommerce_helper_suppress_admin_notices', '__return_true' );
 
 // Additional Functions
 // =============================================================================
+// GRAVITY - Custom Activation Page SUB FOLDER
+/**
+ * Gravity Forms Custom Activation Template
+ * http://gravitywiz.com/customizing-gravity-forms-user-registration-activation-page
+ */
+add_action('wp', 'custom_maybe_activate_user', 9);
+function custom_maybe_activate_user() {
+
+	$template_path = STYLESHEETPATH . '/gfur-activate-template/activate.php';
+	$is_activate_page = isset( $_GET['page'] ) && $_GET['page'] == 'gf_activation';
+
+	if( ! file_exists( $template_path ) || ! $is_activate_page  )
+		return;
+
+	require_once( $template_path );
+
+	exit();
+}
 
 // WOO - Exclude Category from Shop Page
 function custom_pre_get_posts_query( $q ) {
